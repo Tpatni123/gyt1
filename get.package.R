479d0d53085adcad352bce7137392e9215b2d2ec
Tushar Patni <42519735+Tpatni123@users.noreply.github.com>
2024-05-09 21:30:01 -0500

    Update get.package.R

--- get.package.R
+++ get.package.R
@@ -4,7 +4,7 @@ get.package=function(package.name)
   pack.list=installed.packages()
   if(!(package.name%in%pack.list))
   {
-    res=try(install.packages(package.name),silent=T)
+    res=try(webr::install(package.name),silent=T)
     if (class(res)=="try-error")
       stop(paste0("CRAN has no package named '",package.name,"'.  No package installed."))
   }
