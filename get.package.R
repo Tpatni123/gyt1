ee015a70c9ae2f982d573bdf5931b63ece04e3d6
Tushar Patni <42519735+Tpatni123@users.noreply.github.com>
2024-05-09 21:23:17 -0500

    Create get.package.R

---
+++ get.package.R
@@ -0,0 +1,12 @@
+get.package=function(package.name)
+  
+{
+  pack.list=installed.packages()
+  if(!(package.name%in%pack.list))
+  {
+    res=try(install.packages(package.name),silent=T)
+    if (class(res)=="try-error")
+      stop(paste0("CRAN has no package named '",package.name,"'.  No package installed."))
+  }
+  library(as.character(package.name),character.only=T)
+}
